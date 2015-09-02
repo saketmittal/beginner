da452acc127afcbe7e158d00b8befeabf50d3aed
saketmittal <saketmittal.09@gmail.com>
2015-09-02 23:30:50 +0530

    Update 03_simple_statistics.rb

--- 03_simple_statistics.rb
+++ 03_simple_statistics.rb
@@ -3,6 +3,14 @@
 # Hint: use sum_array from before to calculate the sum
 
 def mean(array_num)
+    sum = array_num.inject(0) { |sum, item| sum + item }
+  if array_num.length < 1 then
+    result = 0
+  end
+  if array_num.length > 0 then
+    result = sum / array_num.length
+  end
+  return result
 end
 
 
@@ -16,6 +24,13 @@ p mean([1,2,3,4,5])==3
 # Hint: use sort and %
 
 def median(array_num)
+  sorted = array_num.sort
+
+  if array_num.length % 2 != 0
+    array_num[array_num.length/2]
+  else
+    ((array_num[array_num.length/2] - array_num[(array_num.length/2)-1]).to_f / 2) + array_num[(array_num.length/2)-1]
+  end
 end
 
 p median([1]) == 1
