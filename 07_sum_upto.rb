64f78e3d57c3410069b64b54174326686d3091fd
saketmittal <saketmittal.09@gmail.com>
2015-09-04 19:09:49 +0530

    Update 07_sum_upto.rb

--- 07_sum_upto.rb
+++ 07_sum_upto.rb
@@ -1,7 +1,14 @@
 # Write a method that takes in an integer 'int' and returns the sum upto all integers from 1 upto num
 
 def sum_upto(int)
-
+  sum = 0
+  count = 1
+  #1.upto(int) {|x| sum =+ x}
+  int.times do
+    sum += count
+    count +=1
+  end
+  return sum
 end
 
 p sum_upto(1) == 1
